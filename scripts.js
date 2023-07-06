0445b7e7de3928646d6d4e8f6d89042b3f2c469e
Atty Cronin <antoniajcronin@gmail.com>
2023-07-06 17:45:19 +0100

    User Story 2: Set progress bar value to input value length * 5

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ const input = document.getElementById('password');
 const progressBar = document.getElementById('progress-bar');
 
 const handleInput = () => {
-  const passwordLength = input.value.length + 1;
+  const passwordLength = (input.value.length + 1) * 5;
   progressBar.setAttribute('value', passwordLength);
 };
 input.addEventListener('keydown', handleInput);
